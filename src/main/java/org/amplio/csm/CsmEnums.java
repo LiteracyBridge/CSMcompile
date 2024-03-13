84e05b9000f2f6c6f87fced0707c6236c850673c
billev <billev@users.noreply.github.com>
2024-03-13 15:06:19 -0600

    fix: Change "Home" to "House" and "Pot" to "Bowl", to match common usage.

--- src/main/java/org/amplio/csm/CsmEnums.java
+++ src/main/java/org/amplio/csm/CsmEnums.java
@@ -1,16 +1,9 @@
 package org.amplio.csm;
 
-
-
-
-
-
-
-
 public class CsmEnums{
 
     public enum tknEvent {
-        eNull, Home, Circle, Plus, Minus, Tree, Lhand, Rhand, Pot, Star, Table, Home__, Circle__, Plus__, Minus__, Tree__, Lhand__, Rhand__, Pot__, Star__, Table__, starHome, starCircle, starPlus, starMinus, starTree, starLhand, starRhand, starPot, starStar, starTable, AudioStart, AudioDone, ShortIdle, LongIdle, LowBattery, BattCharging, BattCharged, BattMin, BattMax, FirmwareUpdate, Timer, ChargeFault, LithiumHot, MpuHot, FilesSuccess, FilesFail, OK, CANCEL, anyKey, eUNDEF,
+        eNull, House, Circle, Plus, Minus, Tree, Lhand, Rhand, Bowl, Star, Table, House__, Circle__, Plus__, Minus__, Tree__, Lhand__, Rhand__, Bowl__, Star__, Table__, starHouse, starCircle, starPlus, starMinus, starTree, starLhand, starRhand, starBowl, starStar, starTable, AudioStart, AudioDone, ShortIdle, LongIdle, LowBattery, BattCharging, BattCharged, BattMin, BattMax, FirmwareUpdate, Timer, ChargeFault, LithiumHot, MpuHot, FilesSuccess, FilesFail, OK, CANCEL, anyKey, eUNDEF,
     }
     public enum tknAction {
         aNull, LED, bgLED, playSys, playSubject, playMessage, pausePlay, resumePlay, stopPlay, volAdj, spdAdj, posAdj, startRecording, pauseRecording, resumeRecording, finishRecording, playRecording, saveRecording, writeMsg, resumePrevState, saveState, resumeSavedState, subjAdj, msgAdj, setTimer, resetTimer, showCharge, startUSB, endUSB, powerDown, sysBoot, sysTest, playNxtPkg, changePkg, playTune, filesTest, callScript, exitScript, enterState, beginSurvey, endSurvey, writeRecId,
